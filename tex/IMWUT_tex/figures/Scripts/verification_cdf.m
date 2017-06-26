47611b5111d353925f363146ed131a2393c555bd
Zheng Lu <zlu12@vols.utk.edu>
2017-06-26 12:07:24 -0400

    new

--- tex/IMWUT_tex/figures/Scripts/verification_cdf.m
+++ tex/IMWUT_tex/figures/Scripts/verification_cdf.m
@@ -6,7 +6,7 @@ set(h2, 'LineWidth', 2);
 %set(gca, 'XScale', 'log');
 
 
-legend('errors of raw speed est.', 'erros of filtered speed est.');
+legend('Raw speed estimation', 'Filtered speed estimation', 'Location', 'Northwest');
 set(gca, 'Fontname', 'Times New Roman', 'Fontsize', 20);
 ylabel_hand=ylabel('Cumulative distribution');
-xlabel_hand=xlabel('Error');
+xlabel_hand=xlabel('Speed estimation errors');
